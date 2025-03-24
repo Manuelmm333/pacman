0a82c0e0acc730bb8a90098b08295fe83e5846e3
Manuel Morales <luis_issc@hotmail.com>
2025-03-23 20:46:17 -0600

    basic windows config

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-  std::cout<< "Hello world";
+int main() {
+  auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}, 32), "CMake SFML Project");  
+  while (window.isOpen()) 
+  { 
+    while (const std::optional event = window.pollEvent())
+    {
+        if (event->is<sf::Event::Closed>())
+        {
+            window.close();
+        }
+        else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+        {
+            if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                window.close();
+        }
+    }
+    window.clear(sf::Color::Black);
+    window.display();
+  }
+  
+  return 0;
 }
